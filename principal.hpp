ff892d9ec1c67657e599d8ae2dd149d7efa5a721
mateo <148560902+Matehdez@users.noreply.github.com>
2024-01-11 19:51:12 -0300

    Update principal.hpp

--- principal.hpp
+++ principal.hpp
@@ -1,14 +1,16 @@
-#ifndef _MAINHPP
-#define _MAINHPP
+#define _MAIN
+
 #include <iostream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <string.h>
-#include <stdlib.h>
-#include "biblioteca/funciones/strings.hpp"
-#include "biblioteca/funciones/tokens.hpp"
-#include "biblioteca/tads/Coll.hpp"
+#include <cmath>
+#include "library/funciones/strings.hpp"
+#include "library/funciones/tokens.hpp"
+#include "library/funciones/files.hpp"
+#include "library/tads/Array.hpp"
+#include "library/tads/Map.hpp"
+#include "library/tads/List.hpp"
+#include "library/tads/Stack.hpp"
+#include "library/tads/Queue.hpp"
+#include "library/tads/Coll.hpp"
 using namespace std;
 
 
