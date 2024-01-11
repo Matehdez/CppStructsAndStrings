36f1ade9747c771e54c197804aa356e439a0c114
mateo <148560902+Matehdez@users.noreply.github.com>
2024-01-11 19:55:36 -0300

    Update files.hpp

--- library/funciones/files.hpp
+++ library/funciones/files.hpp
@@ -7,14 +7,14 @@
 template <typename T> void write(FILE* f, T t)
 {
    fseek(f,0,SEEK_CUR);
-   fwrite(&t,sizeof(T),1,f); //escribo 1 elemento del tamaño de sizeof(T)
+   fwrite(&t,sizeof(T),1,f); //I write 1 element of the size of sizeof(T)
 }
 
 template <typename T> T read(FILE* f)
 {
    fseek(f,0,SEEK_CUR);
    T t;
-   fread(&t,sizeof(T),1,f); //escribo 1 elemento del tamaño de sizeof(T)
+   fread(&t,sizeof(T),1,f); //I write 1 element of the size of sizeof(T)
    return t;
 }
 
@@ -25,17 +25,17 @@ template<typename T> void seek(FILE* f, int n)
 
 template<typename T> int fileSize(FILE* f)
 {
-   long int originalPos = ftell(f); // Guarda la posición original del puntero del archivo
-   fseek(f, 0, SEEK_END); // Mueve el puntero del archivo al final
-   long int size = ftell(f); // Obtiene la posición del puntero del archivo, que ahora es el tamaño del archivo
-   fseek(f, originalPos, SEEK_SET); // Restaura la posición original del puntero del archivo
-   return size / sizeof(T); // Retorna la cantidad de registros tipo T que contiene el archivo
+   long int originalPos = ftell(f); // Save the original position of the file pointer
+   fseek(f, 0, SEEK_END); // Move the file pointer to the end
+   long int size = ftell(f); // Gets the position of the file pointer, which is now the size of the file
+   fseek(f, originalPos, SEEK_SET); // Restores the original position of the file pointer
+   return size / sizeof(T); // Returns the number of type T records contained in the file
 }
 
    template<typename T> int filePos(FILE* f)
    {
-      long int pos = ftell(f); // Obtiene la posición actual del puntero del archivo
-      return pos / sizeof(T); // Retorna el número de registro que está siendo apuntado
+      long int pos = ftell(f); // Gets the current position of the file pointer
+      return pos / sizeof(T); // Returns the registration number that is being noted
    }
 
 
