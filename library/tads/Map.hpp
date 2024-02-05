6ae5c0d97f31a28c35023baabdc387c07a8b3f72
mateo <148560902+Matehdez@users.noreply.github.com>
2024-02-05 13:36:00 -0300

    Update Map.hpp

--- library/tads/Map.hpp
+++ library/tads/Map.hpp
@@ -6,246 +6,202 @@
 
 using namespace std;
 
-template <typename K, typename V>
+template<typename K,typename V>
 struct Map
 {
-   Array<K> ke; //keys
-   Array<V> va; //values
-   int num;
-   int cap;
+   Array<K> keys;
+   Array<V> values;
+   int posK;
+   int posV;
 };
 
-template <typename K, typename V>
-Map<K, V> map()
+template<typename K,typename V>
+Map<K,V> map()
 {
-   Map<K, V> m;
-   m.ke = array<K>();
-   m.va = array<V>();
-   m.num = 0;
-   m.cap = 0;
-
+   Map<K,V> m;
+   m.keys = array<K>();
+   m.values = array<V>();
+   m.posK = 0;
+   m.posV = 0;
    return m;
 }
 
-//Returns a pointer to the value associated with the key in the map.
-template <typename K, typename V>
-V* mapGet(Map<K, V> m, K k)
+// Compares two elements
+template<typename T>
+int cmpTT(T a,T b)
 {
-   V* dir = NULL;
-   for( int i = 0; i<arraySize<K>(m.ke); i++ )
-   {
-      if( k==m.ke.arr[i] )
-      {
-         dir = &(m.va.arr[i]);
-      }
-   }
-
-   return dir;
+   return a<b?-1:b<a?1:0;
 }
 
-// Inserts a key-value pair into the map. If the key already exists in the map, it updates its value.
-template <typename K, typename V>
-V* mapPut(Map<K, V>& m, K k, V v)
+// Gets the value associated with a key in the Map
+template<typename K,typename V>
+V* mapGet(Map<K,V> m,K k)
 {
-   V* dir = NULL;
-   bool keyFound = false;
-
-   // If the map is not empty
-   if( m.ke.len!=0 )
+   int pos = arrayFind<K,K>(m.keys,k,cmpTT<K>);
+   if(pos<0)
    {
-      // Find the key on the map
-      for( int i = 0; i<arraySize<K>(m.ke); i++ )
-      {
-         // If the key is found on the map
-         if( k==m.ke.arr[i] )
-         {
-            // Update the value associated with the key
-            arraySet<V>(m.va,i,v);
-            dir = &(m.va.arr[i]);
-            keyFound = true;
-            break;
-         }
-      }
+      return NULL;
    }
-
-   // If the key is not found on the map
-   if( !keyFound )
-   {
-      // Add the new key and value to the map
-      int add = arrayAdd<K>(m.ke,k);
-      arrayAdd<V>(m.va,v);
-      dir = &(m.va.arr[add]);
-   }
-
-   return dir;
+   return arrayGet<V>(m.values,pos);
 }
 
-// Checks if a key is present on the map.
-template <typename K, typename V>
-bool mapContains(Map<K, V> m, K k)
+// Puts a key-value pair in the Map
+template<typename K,typename V>
+V* mapPut(Map<K,V>& m,K k,V v)
 {
-   for( int i = 0; i<arraySize<K>(m.ke); i++ )
+   int pos = arrayFind<K,K>(m.keys,k,cmpTT<K>);
+   if(pos<0)
    {
-      if( k==m.ke.arr[i] )
-      {
-         return true;
-      }
+      arrayAdd<V>(m.values,v);
+      pos = arrayAdd<K>(m.keys,k);
+   }
+   else
+   {
+      arraySet<V>(m.values,pos,v);
+   }
+   
+   return arrayGet<V>(m.values,pos);
+}
+
+// Checks if the Map contains a key
+template<typename K,typename V>
+bool mapContains(Map<K,V> m,K k)
+{
+   return arrayFind<K,K>(m.keys,k,cmpTT<K>)<0?0:1;
+}
+
+// Removes a key-value pair from the Map
+template<typename K,typename V>
+V mapRemove(Map<K,V>& m,K k)
+{
+   int pos = arrayFind<K,K>(m.keys,k,cmpTT<K>);
+   arrayRemove<K>(m.keys,pos);
+   return arrayRemove<V>(m.values,pos);
+}
+
+// Removes all key-value pairs from the Map
+template<typename K,typename V>
+void mapRemoveAll(Map<K,V>& m)
+{
+   arrayRemoveAll<V>(m.values);
+   arrayRemoveAll<K>(m.keys);
+}
+
+// Returns the size of the Map
+template<typename K,typename V>
+int mapSize(Map<K,V> m)
+{
+   return arraySize<K>(m.keys); // mapSize da uno menos, integrar en el codigo fuente.
+}
+
+// Checks if there are more elements in the Map
+template<typename K,typename V>
+bool mapHasNext(Map<K,V> m)
+{
+   if(m.posV<mapSize<K,V>(m) && m.posK<mapSize<K,V>(m))
+   {
+      return true;
    }
    return false;
 }
 
-// Removes a key and its associated value from the map.
-template <typename K, typename V>
-V mapRemove(Map<K, V>& m, K k)
+// Returns the next key in the Map
+template<typename K,typename V>
+K mapNextKey(Map<K,V>& m)
 {
-   V v;
-   bool found = false;
-   for( int i = 0; i<arraySize<K>(m.ke); i++ )
-   {
-      if( k==m.ke.arr[i] )
-      {
-         v = arrayRemove(m.va,i);
-         arrayRemove(m.ke,i);
-         found = true;
-         break;
-      }
-   }
-
-   if( !found )
-   {
-      throw std::invalid_argument("Key not found in map");
-   }
-
-   return v;
-}
-
-// Removes all keys and values ​​from the map.
-template <typename K, typename V>
-void mapRemoveAll(Map<K, V>& m)
-{
-   arrayRemoveAll<K>(m.ke);
-   arrayRemoveAll<V>(m.va);
-}
-
-// Returns the number of elements in the map.
-template <typename K, typename V>
-int mapSize(Map<K, V> m)
-{
-   return arraySize<V>(m.va);
-}
-
-// Checks if there are more elements in the map to iterate.
-template <typename K, typename V>
-bool mapHasNext(Map<K, V> m)
-{
-   return m.num<arraySize(m.va);
-}
-
-// Returns the next key in the map iteration.
-template <typename K, typename V>
-K mapNextKey(Map<K, V>& m)
-{
-   if (m.ke.curr >= arraySize<K>(m.ke)) {
-       throw std::out_of_range("No more keys in map");
-   }
-   K* k;
-   k = arrayGet<K>(m.ke, m.ke.curr++);
+   K* k = arrayGet<K>(m.keys,m.posK);
+   m.posK++;
    return *k;
 }
 
-// Returns the next value in the map iteration.
-template <typename K, typename V>
-V* mapNextValue(Map<K, V>& m)
+// Returns the next value in the Map
+template<typename K,typename V>
+V* mapNextValue(Map<K,V>& m)
 {
-   if( !mapHasNext<K, V>(m) )
+   V* v = arrayGet<V>(m.values,m.posV);
+   m.posV++;
+   return v;
+}
+
+// Resets the positions in the Map
+template<typename K,typename V>
+void mapReset(Map<K,V>& m)
+{
+   m.posV = 0;
+   m.posK = 0;
+}
+
+// Discovers the KEY, if it does not exist it adds it with the value you put. If it finds it, it returns its pointer.
+template<typename K,typename V>
+V* mapDiscover(Map<K,V>& m,K k,V v)
+{
+   if(!mapContains<K,V>(m,k))
    {
-      return nullptr;
+      mapPut<K,V>(m,k,v);
    }
-   V* dir = arrayGet<V>(m.va,m.va.curr++);
-   return dir;
+   return mapGet<K,V>(m,k);
 }
 
-// Restart the map iteration at the beginning.
-template <typename K, typename V>
-void mapReset(Map<K, V>& m)
+// Sorts the Map by keys
+template<typename K,typename V>
+void mapSortByKeys(Map<K,V>& m,int cmpKK(K,K))
 {
-   m.num = 0;
-}
-
-
-
-// Sorts map elements by their keys using a provided compare function.
-template <typename K, typename V>
-void mapSortByKeys(Map<K, V>& m, int cmpKK(K, K))
-{
-   Array<K> kAux = array<K>();
-   Array<V> vAux = array<V>();
-   while( arraySize<K>(m.ke)>0 )
+   bool seInvirtio=true;
+   while(seInvirtio)
    {
-      K* tMin = arrayGet<K>(m.ke,0);
-      int pMin = 0;
-      for( int i = 1; i<arraySize<K>(m.ke); i++ )
+      seInvirtio=false;
+      for(int i=1;i<m.keys.len;i++)
       {
-         K* t1 = arrayGet<K>(m.ke,i);
-         int cmp = cmpKK(*t1,*tMin);
-         if( cmp<0 )
+         if(cmpKK(m.keys.arr[i],m.keys.arr[i-1])<0)
          {
-            tMin = t1;
-            pMin = i;
+            K auxK = m.keys.arr[i-1];
+            V auxV = m.values.arr[i-1];
+            m.keys.arr[i-1] = m.keys.arr[i];
+            m.values.arr[i-1] = m.values.arr[i];
+            m.keys.arr[i] = auxK;
+            m.values.arr[i] = auxV;
+            seInvirtio=true;
          }
       }
-      K removedKey = arrayRemove<K>(m.ke,pMin);
-      V removedValue = arrayRemove<V>(m.va,pMin);
-      arrayAdd<K>(kAux,removedKey);
-      arrayAdd<V>(vAux,removedValue);
    }
-   m.ke = kAux;
-   m.va = vAux;
 }
 
-// Sorts map elements by their values ​​using a provided comparison function.
-template <typename K, typename V>
-void mapSortByValues(Map<K, V>& m, int cmpVV(V, V))
+// Sorts the Map by values
+template<typename K,typename V>
+void mapSortByValues(Map<K,V>& m,int cmpVV(V,V))
 {
-   Array<K> vAux = array<V>();
-   Array<V> kAux = array<K>();
-   while( arraySize<V>(m.va)>0 )
+   bool seInvirtio=true;
+   while(seInvirtio)
    {
-      V* tMin = arrayGet<V>(m.va,0);
-      int pMin = 0;
-      for( int i = 1; i<arraySize<V>(m.va); i++ )
+      seInvirtio=false;
+      for(int i=1;i<m.values.len;i++)
       {
-         K* t1 = arrayGet<V>(m.va,i);
-         int cmp = cmpVV(*t1,*tMin);
-         if( cmp<0 )
+         if(cmpVV(m.values.arr[i],m.values.arr[i-1])<0)
          {
-            tMin = t1;
-            pMin = i;
+            K auxK = m.keys.arr[i-1];
+            V auxV = m.values.arr[i-1];
+            m.keys.arr[i-1] = m.keys.arr[i];
+            m.values.arr[i-1] = m.values.arr[i];
+            m.keys.arr[i] = auxK;
+            m.values.arr[i] = auxV;
+            seInvirtio=true;
          }
       }
-      V removedValue = arrayRemove<V>(m.va,pMin);
-      K removedKey = arrayRemove<K>(m.ke,pMin);
-      arrayAdd<V>(vAux,removedValue);
-      arrayAdd<K>(kAux,removedKey);
    }
-   m.va = vAux;
-   m.ke = kAux;
 }
 
-// If the key is not in the map, inserts the key-value pair and returns a pointer to the value. If the key is already in the map, it returns a pointer to the existing value.
+// Displays all key-value pairs in the Map
 template <typename K, typename V>
-V* mapDiscover(Map<K, V>& m, K k, V v)
+void mapDisplay(Map<K,V> m)
 {
-   V* dir;
-   if( !mapContains<K, V>(m,k) )
-   {
-      dir = mapPut(m,k,v);
-   }
-   else
-   {
-      dir = mapGet<K, V>(m,k);
-   }
-   return dir;
+    K k; V* v = NULL;   
+    while(mapHasNext<K,V>(m))
+    {
+        k = mapNextKey<K,V>(m);
+        cout << "Key " << k;
+        v = mapNextValue<K,V>(m);
+        cout << " has the following value associated with it: " << *v << endl;
+    }
 }
 
 #endif
